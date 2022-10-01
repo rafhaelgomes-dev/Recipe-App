1d8ba84a074ac95ffa2a95471525fd06a6b2a218
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-10-01 20:14:52 -0300

    requisito 46 ajustes testes

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { screen, waitFor, act } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
-import doneRecipesMock from './mocks/doneRecipesMock';
+import doneRecipesMock from './helpers/mocks/doneRecipesMock';
 
 describe('testando Pagina doneRecipes', () => {
   test('Testa se as tags são renderizadas', async () => {
