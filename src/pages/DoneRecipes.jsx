c25595880ba1669dbec19c89520762bfb1fd6340
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-10-04 19:47:59 -0300

    Requisito 48

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -2,10 +2,37 @@ import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
 
+const doneRecipes = [
+  {
+    id: '52771',
+    type: 'meal',
+    nationality: 'Italian',
+    category: 'Vegetarian',
+    alcoholicOrNot: '',
+    name: 'Spicy Arrabiata Penne',
+    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    doneDate: '23/06/2020',
+    tags: ['Pasta', 'Curry'],
+  },
+  {
+    id: '178319',
+    type: 'drink',
+    nationality: '',
+    category: 'Cocktail',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Aquamarine',
+    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+    doneDate: '23/06/2020',
+    tags: [],
+  },
+];
+
 function DoneRecipes() {
   const TITLE_PAGE = 'Done Recipes';
   const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
+    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
     const recipesGetStorage = JSON.parse(localStorage.getItem('doneRecipes'));
     if (recipesGetStorage === null) {
       return;
@@ -13,12 +40,47 @@ function DoneRecipes() {
     setRecipes(recipesGetStorage);
   }, []);
 
+  const handleMeals = () => {
+    const recipesGetStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+    const recipesFilterMeals = recipesGetStorage.filter((e) => e.type === 'meal');
+    setRecipes(recipesFilterMeals);
+  };
+
+  const handleDrinks = () => {
+    const recipesGetStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+    const recipesFilterMeals = recipesGetStorage.filter((e) => e.type === 'drink');
+    setRecipes(recipesFilterMeals);
+  };
+
+  const handleAll = () => {
+    const recipesGetStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+    setRecipes(recipesGetStorage);
+  };
+
   return (
     <div>
       <Header title={ TITLE_PAGE } />
-      <button type="button" data-testid="filter-by-all-btn">All</button>
-      <button type="button" data-testid="filter-by-meal-btn">Meals</button>
-      <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ handleAll }
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-meal-btn"
+        onClick={ handleMeals }
+      >
+        Meals
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ handleDrinks }
+      >
+        Drinks
+      </button>
       {recipes.map((e, i) => (
         <div key={ i }>
           <img
